5efb8b988d5965b9a8b66254a906d54af6d74344
Maxwell Morais <mmorais@aptitudetechinfo.net>
2017-08-31 23:15:07 +0000

    SHEI Accounts Payable US

---
+++ shei/shei/report/accounts_payable_us_shei/accounts_payable_us_shei.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2013, Aptitude technologie and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+from shei.shei.report.accounts_receivable_us_shei import ReceivablePayableReport
+
+def execute(filters=None):
+	args = {
+		"party_type": "Supplier",
+		"naming_by": ["Buying Settings", "supp_master_name"]
+	}
+
+	return ReceivablePayableReport(filters).run(args)
