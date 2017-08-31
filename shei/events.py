1c81ab48a3c1de9a4731862b4a18e1c77cbebad2
Maxwell Morais <mmorais@aptitudetechinfo.net>
2017-08-31 02:34:26 +0000

    Minor fix

--- shei/events.py
+++ shei/events.py
@@ -9,7 +9,7 @@ def on_customer_validate(doc, handler=None):
     if not doc.get('customer_code'):
         doc.customer_code = frappe.db.get_value(
             'Custom Series',
-            {'name': doc.name},
+            {'name': 'Customer'},
             'series'
         )
 
@@ -17,7 +17,7 @@ def on_customer_after_insert( doc, handler=None ):
     if doc.customer_code:
         frappe.db.set_value(
             'Custom Series',
-            {'name': doc.name},
+            {'name': 'Customer'},
             'series',
             doc.customer_code + 1
         )
