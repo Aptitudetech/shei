301dd583507d179d81dc23fbf4d8b0697849f09b
Maxwell Morais <mmorais@aptitudetechinfo.net>
2017-10-02 14:56:46 +0000

    Fix button call using controller inheritance

--- shei/public/js/shei.js
+++ shei/public/js/shei.js
@@ -1,20 +1,25 @@
-
-cur_frm.cscript.get_credit_notes = function(frm, cdt, cdn){
-	frappe.call({
-		"method": "shei.events.get_credit_notes",
-		"args": {
-			"doctype": frm.doctype,
-			"party_type": "customer",
-			"part_name": frm.doc.customer
-		},
-		"callback": function(res){
-			if(res && res.message){
-				frm.clear_table("credits");
-				res.message.forEach(function(row){
-					var d = frm.add_child('credits', row)
-				});
-				frm.refresh_field('credits');
-			}
+erpnext.accounts.SalesInvoiceController = erpnext.accounts.SalesInvoiceController.extend({
+	"get_credit_notes": function(frm){
+		if (frm.doc.customer){
+			return frm.call({
+				"method": "shei.events.get_credit_notes",
+				"args": {
+					"doctype": frm.doctype,
+					"party_type": "customer",
+					"party_name": frm.doc.customer
+				},
+				"callback": function(res){
+					if (res && res.message){
+						frm.clear_table('credits');
+						res.message.forEach(function(row){
+							var d = frm.add_child('credits', row);
+						});
+						frm.refresh_field("credits");
+					}
+				}
+			});
 		}
-	});
-}
+	}
+});
+
+$.extend(cur_frm.cscript, new erpnext.accounts.SalesInvoiceController({frm: cur_frm}));
