4115b5503bab8aa66bbd817187fa0fdfd1e9d3f5
Bernard Lefebvre <blefebvre@aptitudetech.net>
2018-12-17 19:32:06 +0000

    modified permissions

---
+++ shei/permissions.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+import frappe
+
+def get_product_configurator_permissions_query_conditions(user):
+        if not user:
+                user = frappe.session.user
+
+        if 'SHEI - Product Configurator User' in frappe.get_roles(user):
+                pcu = frappe.db.exists("Product Configurator", {"user_email": user})
+                if pcu:
+                        return '''(`tabProduct Configurator`.name = "{pcu}")'''.format(pcu=frappe.db.escape(pcu))
+
+
+def has_permission_to_product_configurator(doc, user):
+        if 'SHEI - Product Configurator User' in frappe.get_roles(user):
+                pcu = frappe.db.exists("Product Configurator", { "user_email": user })
+                if pcu:
+                        return doc.name == pcu
+
