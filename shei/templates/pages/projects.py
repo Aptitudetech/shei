f194705272cea860a583d53cbd7baf073cd6de3f
Bernard Lefebvre <blefebvre@aptitudetech.net>
2019-05-23 19:01:07 +0000

    Add Shipping into the Customer Portal

--- shei/templates/pages/projects.py
+++ shei/templates/pages/projects.py
@@ -5,12 +5,11 @@ from __future__ import unicode_literals
 import frappe
 import json
 from frappe import _
+import datetime as dt
 
 @frappe.whitelist()
 def upload_wetransfer_link(doc_name, link):
 	from frappe.email.doctype.email_template.email_template import get_email_template
-	frappe.msgprint(_("Hey"))
-	frappe.msgprint(_("Hey  --  {0}  --  {1}").format(doc_name, link))
 	recipient = frappe.get_value('Project', doc_name, 'project_manager')
 	#recipient =  frappe.get_value('User', project_manager, 'email')
 	recipient = 'melissaraymond48@gmail.com'
@@ -21,3 +20,37 @@ def upload_wetransfer_link(doc_name, link):
 		subject="Files Received for project {0}".format(doc_name), 
 		content=content
 	)
+
+@frappe.whitelist()
+def update_shipping_address(doc_name, customer, address_title, address_line_1, address_city, address_country):
+	frappe.msgprint(_("Hey  --  {0}  --  {1} --  {2} --  {3}").format(address_title, address_line_1, address_city, address_country))
+	address = frappe.new_doc('Address')
+	address.update({
+		"address_title": address_title,
+		"address_line1": address_line_1,
+		"city": address_city,
+		"country": address_country,
+		"address_type": 'Shipping',
+		"links": [{
+			'link_doctype': 'Customer',
+			'link_name': customer,
+		}],
+	})
+	address.flags.ignore_permissions = True
+	address.save()
+	frappe.msgprint(_("name: {0}").format( address.name ))
+	sales_orders = frappe.get_list('Sales Order', filters={'project': doc_name, 'status': ['IN', ['Completed', 'To Bill', 'To Deliver and Bill']]}, fields=['name'])
+	for so in sales_orders:
+		sales_order = frappe.get_doc('Sales Order', so.name)
+		sales_order.update({
+			"shipping_address_name": address.name,
+		})
+		sales_order.flags.ignore_permissions = True
+		sales_order.save()
+		frappe.msgprint(_("sales_order: {0}").format( sales_order.name ))
+
+#look if already exist add name + dropdown for country + allow to see existing address (could be a good idea)
+
+@frappe.whitelist()
+def nearest(items, curr_date):
+    return min(items, key=lambda x: abs(x - curr_date))
