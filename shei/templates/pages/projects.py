be342dc043b71e0f17d66162e6d6cc07580082f0
Bernard Lefebvre <blefebvre@aptitudetech.net>
2019-06-06 18:51:24 +0000

    Finalizing the Customer Portal

--- shei/templates/pages/projects.py
+++ shei/templates/pages/projects.py
@@ -6,9 +6,11 @@ import frappe
 import json
 from frappe import _
 import datetime as dt
+import re
 
 @frappe.whitelist()
 def upload_wetransfer_link(doc_name, link):
+	"""Send the WeTransfer link by emil to the project manager"""
 	recipient = frappe.get_value('Project', doc_name, 'project_manager')
 	recipient = 'melissaraymond48@gmail.com'
 	content = _('<p>Hi,</p><p>You received some files from <a href="https://en-dev.shei.sh/desk#Form/Project/{0}".format(doc_name) target="_blank">{0}</a></p><p>To see them, please click on this link: <a href="{1}".format(link) target="_blank">WeTransfer</a></p>').format(doc_name, link)
@@ -17,6 +19,7 @@ def upload_wetransfer_link(doc_name, link):
 
 @frappe.whitelist()
 def update_address_information(address_list = []):
+	"""Update some information in each address related to the account"""
 	for address in address_list:
 		address = frappe.get_doc('Address', address['address_name']).update({'is_residential_address': address['is_residential'], 'have_dock': address['have_dock']})
 		address.flags.ignore_permissions = True
@@ -32,13 +35,14 @@ def send_email(recipient, subject, content):
 	)
 
 @frappe.whitelist()
-def create_shipping_address(address_title, address_line_1, address_city, address_country, customer):
+def create_shipping_address(address_title, address_line_1, address_city, address_zipcode, address_country, customer):
 	address = frappe.new_doc('Address')
 	address.update({
 		"address_title": address_title,
 		"address_line1": address_line_1,
 		"city": address_city,
 		"country": address_country,
+		"pincode": address_zipcode,
 		"address_type": 'Shipping',
 		"links": [{
 			'link_doctype': 'Customer',
@@ -50,16 +54,17 @@ def create_shipping_address(address_title, address_line_1, address_city, address
 	return address
 
 @frappe.whitelist()
-def update_shipping_address(doc_name, sales_order_name, customer, address_line_1, address_city, address_country, address_title, checked_address_value, address_info_list = []):
-	address_validation(address_title, address_line_1, address_city, address_country, checked_address_value)
+def update_shipping_address(doc_name, sales_order_name, customer, address_line_1, address_city, address_zipcode, address_country, address_title, checked_address_value, address_info_list = []):
+	address_validation(address_title, address_line_1, address_city, address_country, address_zipcode, checked_address_value)
 	address_list = json.loads(address_info_list)
 	update_address_information(address_list)
-	if address_title:
-		new_address = create_shipping_address(address_title, address_line_1, address_city, address_country, customer)
+	if checked_address_value == 'Other':
+		new_address = create_shipping_address(address_title, address_line_1, address_city, address_zipcode, address_country, customer)
 		shipping_address = new_address.name
 	else:
 		shipping_address = checked_address_value
-	#send email
+	if checked_address_value == frappe.db.get_value('Sales Order', sales_order_name, 'shipping_address_name'):
+		return
 	recipient = frappe.get_value('Project', doc_name, 'project_manager')
 	recipient = 'melissaraymond48@gmail.com'
 	content = _('<p>Hi,</p><p>{0} would like to change his shipping address for the Sales Order <a href="https://en-dev.shei.sh/desk#Form/Sales%20Order/{1}" target="_blank">{1}</a></p> \
@@ -69,8 +74,57 @@ def update_shipping_address(doc_name, sales_order_name, customer, address_line_1
 	frappe.msgprint(_("Your request have been sent to your project manager."))
 
 @frappe.whitelist()
-def address_validation(address_title, address_line_1, address_city, address_country, checked_address_value):
-	if((checked_address_value == 'Other') and (not address_title or not address_line_1 or not address_city or not address_country)):
+def address_validation(address_title, address_line_1, address_city, address_country, address_zipcode, checked_address_value):
+	if((checked_address_value == 'Other') and (not address_title or not address_line_1 or not address_city or not address_country or not address_zipcode)):
 		frappe.throw(_("You must filled all mandatory fields for the new address"))
 	if frappe.db.exists('Address', "{0}-Shipping".format(address_title)):
 		frappe.throw(_("Sorry, this address title already exist. Please choose something else"))
+
+@frappe.whitelist()
+def create_contact(customer, contact_first_name, contact_last_name, contact_email, contact_phone):
+	contact = frappe.new_doc('Contact')
+	contact.update({
+		"first_name": contact_first_name,
+		"last_name": contact_last_name,
+		"email_id": contact_email,
+		"phone": contact_phone,
+		"links": [{
+			'link_doctype': 'Customer',
+			'link_name': customer,
+		}],
+	})
+	contact.flags.ignore_permissions = True
+	contact.save()
+	return contact
+
+@frappe.whitelist()
+def update_contact(doc_name, customer, contact_first_name, contact_last_name, 
+					contact_email, contact_phone, contact_mobile, contact_department, 
+					contact_title, checked_contact_value, so_name):
+	#there's some Sales Order who don't have a contact. When updating the address, this method is also trigger.
+	#We don't want to process the method if the value haven't changed
+	if not checked_contact_value or checked_contact_value == frappe.db.get_value('Sales Order', so_name, 'contact_display'):
+		return
+	contact_validation(customer, contact_first_name, contact_last_name, contact_email, contact_phone, checked_contact_value)
+	if checked_contact_value == 'Other':
+		new_contact = create_contact(customer, contact_first_name, contact_last_name, contact_email, contact_phone)
+		contact_name = new_contact.name
+		contact_full_name = "{0} {1}".format(contact_first_name, contact_last_name)
+	else:
+		contact_name = "{0}-{1}".format(checked_contact_value, customer)
+		contact_full_name = checked_contact_value
+	recipient = frappe.get_value('Project', doc_name, 'project_manager')
+	recipient = 'melissaraymond48@gmail.com'
+	content = _('<p>Hi,</p><p>{0} would like to change his contact for the Sales Order <a href="https://en-dev.shei.sh/desk#Form/Sales%20Order/{1}" target="_blank">{1}</a></p> \
+	<p>New Contact: <a href="https://en-dev.shei.sh/desk#Form/Contact/{2}" target="_blank">{2}</a></p>').format(customer, so_name, contact_name)
+	subject = "Change Contact for Project {0}".format(doc_name)
+	send_email(recipient, subject, content)
+	frappe.msgprint(_("Your request have been sent to your project manager."))
+
+@frappe.whitelist()
+def contact_validation(customer, contact_first_name, contact_last_name, contact_email, contact_phone, checked_contact_value):
+	if((checked_contact_value == 'Other') and (not contact_first_name or not contact_last_name or not contact_email or not contact_phone)):
+		frappe.throw(_("You must filled all mandatory fields for the new Contact"))
+	email = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', contact_email)
+	if((checked_contact_value == 'Other') and (not email)):
+		frappe.throw(_("The given email is invalid: {0}").format(contact_email))
